7d75140523a689225874d07049881b7b727a30ec
JULIENIS BRINEZ <julebrz@gmail.com>
2022-06-03 22:25:11 -0400

    initial commit

---
+++ .history/public/scripts/app_20220518141127.js
@@ -0,0 +1,33 @@
+//IIFE = anonimous funtion
+(function()
+    {
+        function start()
+        {
+            console.log("app started!");
+
+            //use AJAX to read json file
+
+            //step 1: create a new XHR object
+            let XHR = new XMLHttpRequest(); //exe html request
+
+            // step 2: create the request
+            XHR.open("GET","../data/data.json");
+
+            //step 3: send the request
+            XHR.send();
+
+            //step 4: create an event listener/handler
+            XHR.addEventListener("readystatechange", function()
+            {
+                if(XHR.readyState === "4" && XHR.status == "200")
+                {
+                    console.log("JSON Data:");
+                    console.log("++++");
+                    console.log(XHR.responseText);
+                }
+            })
+
+        }
+
+        window.addEventListener("load", start)
+    })();
---
+++ .history/server_20220525132413.ts
@@ -0,0 +1,53 @@
+// require built-in modules
+const http = require('http');
+const fs = require ('fs');
+const mime = require('mime-types');
+const port = 3000;
+
+let lookup = mime.lookup; //alias for mime.lookup
+
+// when the server is instantiated (created) it is IMMUTABLE
+const server = http.createServer(function(req, res)
+{
+
+    let path = req.url; // alias for req.url
+
+    if(path === "/" || path === "/home")
+    {
+        path = "/index.html";
+    }
+
+    let mime_type = lookup(path.substring(1));
+    console.log(`MIME TYPE: ${mime_type}`);
+
+    //reads a file from the file system
+    fs.readFile(__dirname + path, function (err, data)
+    {
+        //if there is an error existing with the url
+        if(err)
+        {
+            res.writeHead(404); //file not found
+            console.log(`ERROR: ${err.message}`);
+            return res.end("ERROR: 404");
+            
+        }
+
+        // no error exist
+        res.setHeader("X-Content-Type-Options", "nosniff");
+
+        res.writeHead(200); //all ok
+        console.log(`full file name: ${__dirname}${req.url}`)
+        return res.end(data); //outputs the file to the browser
+
+    })
+    //this code bellow is another way to do that above ^
+    //this next console log is showing only in console not in browser
+    //console.log("hello, console");
+    //console.log("Directory Name: " + __dirname);
+});
+
+server.listen(port, function()
+{
+    console.log("Server Running at Port: " + port);
+});
+
